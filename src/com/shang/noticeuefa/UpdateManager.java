6312fc493ece2a0ab6e643756ae5439e959610a8
jleo <ggyyleo@gmail.com>
2012-05-05 18:59:07 +0800

    add umeng

--- src/com/shang/noticeuefa/UpdateManager.java
+++ src/com/shang/noticeuefa/UpdateManager.java
@@ -1,5 +1,10 @@
 package com.shang.noticeuefa;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import com.mobclick.android.MobclickAgent;
+
 /**
  * Created with IntelliJ IDEA.
  * User: jleo
@@ -8,7 +13,26 @@ package com.shang.noticeuefa;
  * To change this template use File | Settings | File Templates.
  */
 public class UpdateManager {
-    public void checkUpdate(){
+    private Activity activity;
 
+    public UpdateManager(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void checkUpdate() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                MobclickAgent.updateOnlineConfig(activity);
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(activity);
+                String host = MobclickAgent.getConfigParams(activity, activity.getString(R.string.umeng_host_key));
+                if (host != null && host.length() > 0) {
+                    preferences.edit().putString(activity.getString(R.string.key_tika_host), host).commit();
+                } else {
+                    host = preferences.getString(activity.getString(R.string.key_tika_host), Constants.TIKA_HOST);
+                }
+                Constants.TIKA_HOST = host;//
+            }
+        }).start();
     }
 }
